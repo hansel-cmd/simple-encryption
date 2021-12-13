7fff32f518e2807ce704a43a41dc3610f09b672c
Hansel <valkirecesa@gmail.com>
2021-12-13 23:31:25 +0800

    ~ finish

--- index.py
+++ index.py
@@ -37,5 +37,54 @@ class Encryption():
         return self.alphabets[(index + self.vowel_count) % len(self.alphabets)]
         
 
+class Decryption():
+
+    alphabets = 'abcdefghijklmnopqrstuvwxyz'
+    vowels = "aeiouAEIOU"
+
+    def __init__(self): pass
+
+    def count_vowels(self, word):
+        count = sum((*map(word.count, "AEIOU"), 0))
+        return count if count else 13
+
+    def decrypt(self, message):
+        word_list = message.split(' ')
+
+        decrypted_message = []
+        for word in word_list:
+            self.vowel_count = self.count_vowels(word)
+            decrypted_message.append(''.join(self.rotate(word)))
+        
+        return ' '.join(decrypted_message)
+
+    def rotate(self, word):
+        rotated_word = []
+        for char in word:
+            if char.isalpha():
+                if char.islower():
+                    rotated_word.append(self.swap_char(char))
+                else:
+                    rotated_word.append(char.lower())
+            else:
+                rotated_word.append(char)
+
+        return rotated_word
+
+    def swap_char(self, char):
+        index = self.alphabets.find(char)
+        padding = index - self.vowel_count
+        if padding < 0:
+            padding = len(self.alphabets) + padding
+        return self.alphabets[(padding)]
+
+
+
+
 me = Encryption()
-print(me.encrypt("Drink some water, reload your mags, and let's get back out there."))
+print(me.encrypt("If I Can't Save The One Small Girl In Front Of Me, How Can I Become A Hero That Saves Everyone?"))
+
+you = Decryption()
+print(you.decrypt("Ig I dAo'u uAxE uiE OpE tnAmm hIsm Io gsOou Og nE, iOx dAo I eEfOpE A jEtO uiAu uAxEu EzEvcOrE?"))
+
+ 
